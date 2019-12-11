44f90da7576ced9ce261ddf1667ca3acf1802dfc
jnjoe <58746766+jnjoe@users.noreply.github.com>
2019-12-11 14:45:08 +0545

    Add files via upload
    Day 3

---
+++ return_statement.py
@@ -0,0 +1,7 @@
+def cube(num):
+    return num*num*num
+    #no command can be put after return keyword as after return statement the function breaks to give the return data
+    
+result = cube(5)
+print(cube(3))    
+print(result)
